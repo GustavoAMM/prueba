71b1a4b13e44c14505abf0793633d6484619e085
Angel Montoya <j.gustavo.m245@gmail.com>
2023-08-07 00:10:28 -0600

    fix style - updatedata student

--- resources/views/teacher/updateStudents.blade.php
+++ resources/views/teacher/updateStudents.blade.php
@@ -3,14 +3,15 @@
 @section('title', 'Editar Notas del Estudiante')
 
 @section('content')
-<div class="max-w-lg mx-auto py-10">
-    <h1 class="text-3xl font-bold mb-6">Editar Notas del Estudiante</h1>
-    <div class="bg-white shadow-md rounded-lg p-6 space-y-4">
-        <form action="{{ route('student.update', $student->id) }}" method="post">
-            @csrf
-            @method('PUT')
+<div class="flex justify-center items-center bg-gray-100">
+    <div class="max-w-md w-full bg-white shadow-lg rounded-lg p-8 space-y-6 overflow-y-auto">
+        <h1 class="text-3xl font-bold text-center mb-6">Editar Notas del Estudiante</h1>
+        <div class="bg-white shadow-md rounded-lg p-6 space-y-4">
+            <form action="{{ route('student.update', $student->id) }}" method="post">
+                @csrf
+                @method('PUT')
 
-            <div class="mb-4">
+                <div class="mb-4">
                 <label for="name" class="block text-lg font-semibold mb-2">Nombre</label>
                 <input type="text" name="name" id="name" class="w-full px-4 py-2 border rounded-md"  value="{{ $student->user->name }}">
                 @error('name')
@@ -57,9 +58,12 @@
                 @enderror
             </div>
 
-            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Guardar Notas</button>
-        </form>
+
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-full">Guardar Notas</button>
+            </form>
+        </div>
+        <div class="border-t border-gray-300 mt-6"></div>
+        <a href="{{ route('teachers') }}" class="block mt-6 text-blue-500 hover:underline text-center">Volver</a>
     </div>
-    <a href="{{ route('teachers') }}" class="block mt-6 text-blue-500 hover:underline">Volver</a>
 </div>
 @endsection
