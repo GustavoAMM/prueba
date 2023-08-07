268602bea6106800f58709eae200f39f8b2856ee
Angel Montoya <j.gustavo.m245@gmail.com>
2023-08-07 00:06:34 -0600

    fix style - showdata student

--- resources/views/teacher/showStudents.blade.php
+++ resources/views/teacher/showStudents.blade.php
@@ -3,17 +3,20 @@
 @section('title', 'Detalles del Estudiante')
 
 @section('content')
-<div class="max-w-lg mx-auto py-10">
-    <h1 class="text-3xl font-bold mb-6">Detalles del Estudiante</h1>
-    <div class="bg-white shadow-md rounded-lg p-6 space-y-4">
-        <ul>
-            <li><strong>Nombre:</strong> {{ $student->user->name }} {{ $student->user->lastname }}</li>
-            <li><strong>Matrícula:</strong> {{ $student->enrollment }}</li>
-            <li><strong>Generación:</strong> {{ $student->generation }}</li>
-            <li><strong>Aula:</strong> {{ $student->classroom }}</li>
-            <li><strong>Promedio Final:</strong> {{ $student->final_grade }}</li>
-        </ul>
+<div class="flex justify-center items-center bg-gray-100">
+    <div class="max-w-lg w-full bg-white shadow-lg rounded-lg p-8 space-y-6">
+        <h1 class="text-3xl font-bold text-center mb-6">Detalles del Estudiante</h1>
+        <div class="bg-white shadow-md rounded-lg p-6 space-y-4">
+            <ul>
+                <li><strong>Nombre:</strong> {{ $student->user->name }} {{ $student->user->lastname }}</li>
+                <li><strong>Matrícula:</strong> {{ $student->enrollment }}</li>
+                <li><strong>Generación:</strong> {{ $student->generation }}</li>
+                <li><strong>Aula:</strong> {{ $student->classroom }}</li>
+                <li><strong>Promedio Final:</strong> {{ $student->final_grade }}</li>
+            </ul>
+        </div>
+        <div class="border-t border-gray-300 mt-6"></div>
+        <a href="{{ route('teachers') }}" class="block mt-6 text-blue-500 hover:underline text-center">Volver</a>
     </div>
-    <a href="{{ route('teachers') }}" class="block mt-6 text-blue-500 hover:underline">Volver</a>
 </div>
 @endsection
