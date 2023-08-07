653f409e53fb426431d6b29c25d2c38a69628152
Angel Montoya <j.gustavo.m245@gmail.com>
2023-08-06 23:58:01 -0600

    fix style - student

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -3,19 +3,25 @@
 @section('title', 'Estudiante')
 
 @section('content')
-<div class="max-w-lg mx-auto py-10">
-    <h1 class="text-3xl font-bold mb-4">Estudiante</h1>
-    <div class="bg-white shadow-md rounded-lg p-6">
-        <h2 class="text-xl font-semibold mb-2">Bienvenido {{ $user->name }} {{ $user->lastname }}</h2>
-        <h3 class="text-lg font-semibold mb-4">Estudiante: {{ $estudiante->enrollment }}</h3>
-        <p><strong>Email:</strong> {{ $user->email }}</p>
-        <p><strong>Generación:</strong> {{ $estudiante->generation }}</p>
-        <p><strong>Aula:</strong> {{ $estudiante->classroom }}</p>
-        <p><strong>Promedio Final:</strong> {{ $estudiante->final_grade }}</p>
+<div class="flex justify-center items-center bg-gray-100">
+    <div class="max-w-md w-full bg-white shadow-lg rounded-lg p-8 space-y-6">
+        <h1 class="text-3xl font-bold text-center mb-4">¡Bienvenido, {{ $user->name }} {{ $user->lastname }}!</h1>
+        <div class="border-t border-gray-300"></div>
+        <div class="space-y-2">
+            <p class="text-lg"><strong>Información del Estudiante:</strong></p>
+            <ul class="list-disc list-inside">
+                <li><strong>Matrícula:</strong> {{ $estudiante->enrollment }}</li>
+                <li><strong>Email:</strong> {{ $user->email }}</li>
+                <li><strong>Generación:</strong> {{ $estudiante->generation }}</li>
+                <li><strong>Aula:</strong> {{ $estudiante->classroom }}</li>
+                <li><strong>Promedio Final:</strong> {{ $estudiante->final_grade }}</li>
+            </ul>
+        </div>
+        <div class="border-t border-gray-300"></div>
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 w-full">Cerrar Sesión</button>
+        </form>
     </div>
-    <form action="{{ route('logout') }}" method="post" class="mt-6">
-        @csrf
-        <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600">Salir</button>
-    </form>
 </div>
 @endsection
